c0c621b68174c41c7796257bef8459d8ffcf604e
Rochak Siwach <rochaksiwach13@gmail.com>
2026-04-05 12:53:05 +0530

    Auth Pages

--- codeSphere/src/auth/login.jsx
+++ codeSphere/src/auth/login.jsx
@@ -1,48 +1,43 @@
 import React from 'react'
 import './login.css'
-import Logo from '../assets/CodeSphere.png'
 import login_Logo from '../assets/Login.png'
 
 const Login = () => {
   return (
     <div className='login'>
+      <div className='imgBlock'>
+        <img src={login_Logo} alt="Login visual" />
+      </div>
 
-        <div className='imgBlock'>
-            {/* <h1>Code Sphere</h1> */}
-            <img src={login_Logo} alt="" />
-            {/* <img src={Logo} alt="CodeSphere logo" /> */}
-        </div>
-
-        <div className='loginBlock'>
-
+      <div className='loginBlock'>
         <h1>Login to Code Sphere</h1>
 
-            <form action="/login" method="post">
-            
-            <label htmlFor="email">Username or email address</label> 
-            <input 
-                type="email" 
-                placeholder='Enter your email here...'
-                />
-                <br />
+        <form action="/login" method="post">
+          <label htmlFor="email">Username or email address</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            placeholder="Enter your email here..."
+          />
 
-            <label htmlFor="password">Password</label> 
-            <input 
-                type="text" 
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            placeholder="Enter your password"
+          />
 
-                /> 
+          <button type="submit">Sign In</button>
 
-            <button type="submit">Sign In</button>  
+          <hr />
 
-            <hr />
-
-            New to Code Sphere ?<a href="/signUp">Sign Up</a>
-
-            </form>
-            
-
-
-        </div>
+          <p className="signupText">
+            New to Code Sphere? <a href="/signUp">Sign Up</a>
+          </p>
+        </form>
+      </div>
     </div>
   )
 }

126ef1ad285e13e038638f491a0b0d7f1d04b812
Rochak Siwach <rochaksiwach13@gmail.com>
2026-04-11 00:16:24 +0530

    LandingPage Updated

--- codeSphere/README.md
+++ codeSphere/README.md
@@ -1,16 +1,7 @@
-# React + Vite
+# Code-Sphere
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Oxc](https://oxc.rs)
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/)
-
-## React Compiler
-
-The React Compiler is not enabled on this template because of its impact on dev & build performances. To add it, see [this documentation](https://react.dev/learn/react-compiler/installation).
-
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+Real-Time Collaborative Coding Platform 
+• Built a real-time collaborative coding platform for multiple users with live editing and cursor tracking.
+• Implemented live code synchronization, syntax highlighting, and real-time chat using WebSockets.
+• Developed responsive React.js front-end and Node.js/Express.js backend for session management.
+• Integrated video calling with WebRTC for live collaboration.
--- codeSphere/src/components/LandingPage.jsx
+++ codeSphere/src/components/LandingPage.jsx
@@ -1,29 +1,27 @@
 import React, { useState } from "react";
-import './landing.css'
+import "./landing.css";
+import chat from "../assets/chat.png";
+import editor from "../assets/Editor.png";
 
-const welcomePage = () => {
-  const [view, setView] = useState("one");
+const LandingPage = () => {
+  const [view, setView] = useState("chat");
 
   return (
     <>
-      <div className="pageOne" style={{ textAlign: "center" }}>
-        <h3 className="pageOne-heading" style={{ fontSize: "5rem" }}>Code, chat and collaborate in </h3>
-        <h1 style={{ fontSize: "4rem" }}>one place.</h1>
-        <p>
+      {/* page one */}
+      <div className="pageOne">
+        <h3 className="pageOne-heading">Code, chat and collaborate in </h3>
+        <h1>one place.</h1>
+        <p className="description">
           Code Sphere is a real-time collaborative coding platform where
           developers, students, and teams can write code together, communicate
           instantly, and build projects faster.
         </p>
 
         {/* inout signup and source code  */}
-        <div
-          style={{
-            marginTop: "30px",
-            display: "flex",
-            justifyContent: "center",
-          }}
-        >
+        <div className="connectCollection">
           <div
+            className="inputBox"
             style={{
               border: "1px solid black",
               padding: "10px",
@@ -39,49 +37,155 @@ const welcomePage = () => {
               name="email"
               id="email"
               placeholder="Enter your email"
-              style={{
-                padding: "8px",
-                border: "none",
-                outline: "none",
-                fontSize: "1rem",
-              }}
+              className="emailInput"
             />
-            <a
-              href="/signup"
-              style={{
-                textDecoration: "none",
-                backgroundColor: "black",
-                color: "white",
-                padding: "8px 16px",
-                borderRadius: "5px",
-              }}
-            >
+            <a href="/signup" className="signUp">
               Join now
             </a>
           </div>
-          <a
-            href="https://github.com/RochakSiwach"
-            style={{
-              background: "black",
-              width: "fit-content",
-              color: "white",
-              height: "60px",
-              padding: "10px",
-              margin: "10px",
-              borderRadius: "10px",
-              alignContent: "center",
-            }}
-          >
+          <a href="https://github.com/RochakSiwach" className="sourceCode">
             Our Source Code
           </a>
         </div>
-        <div>
+        <div className="built-with">
           <p>Built with</p>
-          react, mongo, css, nodejs, express.js
+          <div className="tech">
+            <a href="https://react.dev/">React.js</a> <hr />
+            <a href="https://nodejs.org/en">Node.js</a> <hr />
+            <a href="https://www.mongodb.com/">MongoDB</a> <hr />
+            <a href="https://socket.io/">Socket.io</a>
+          </div>
         </div>
       </div>
+
+
+      {/* page Two */}
+      <div className="pageTwo">
+        <section className="features">
+          <h2>Features</h2>
+
+          <div className="featureBoxButton">
+            <button
+              onClick={() => {
+                setView("chat");
+              }}
+            >
+              Real-Time Chat Collaboration
+            </button>
+            <button
+              onClick={() => {
+                setView("connections");
+              }}
+            >
+              Live Connections
+            </button>
+            <button
+              onClick={() => {
+                setView("code");
+              }}
+            >
+              Code Editor
+            </button>
+          </div>
+          <div className="featureBox">
+            {view === "chat" && (
+              <div className="featureContent">
+                <div className="content">
+                  <h3>Real-Time Chat Collaboration</h3>
+                  <p>Work on code together instantly.</p>
+                </div>
+                <img src={chat} alt="Real-Time_Chat_Collaboration_Image" />
+              </div>
+            )}
+
+            {view === "connections" && (
+              <div className="featureContent">
+                <div className="content">
+                  <h3>Live Connection</h3>
+                  <p>Work on code together instantly.</p>
+                </div>
+                <img src="" alt="Live_Connection_Image" />
+              </div>
+            )}
+
+            {view === "code" && (
+              <div className="featureContent">
+                <div className="content">
+                  <h3>Code Editor</h3>
+                  <p>Syntax highlighting and live updates.</p>
+                </div>
+                <img src={editor} alt="Code_Editor_Image" />
+              </div>
+            )}
+          </div>
+        </section>
+      </div>
+
+
+      {/* page Three */}
+          <section className="usecases">
+      <h2>Who is it for?</h2>
+
+<div className="usecaseGrid">
+  <div className="usecaseCard">
+    <h3>👨‍💻 Developers</h3>
+    <ul>
+      <li>Build, debug, and ship code together in real-time.</li>
+      <li>Collaborate seamlessly
+      with your team.</li>
+      <li>Share ideas instantly, and accelerate your development
+      workflow without switching between tools.</li>
+
+    </ul>
+
+  </div>
+
+  <div className="usecaseCard">
+    <h3>🎓 Students</h3>
+    <ul>
+      <li>Learn coding interactively with friends and classmates.</li>
+      <li>Practice problems.</li>
+      <li>Work on assignments.</li>
+      <li>And get instant help while coding together in a
+      shared environment.</li>
+    </ul>
+  </div>
+
+  <div className="usecaseCard">
+    <h3>🧑‍💼 Interviewers</h3>
+   <ul>
+      <li>Conduct efficient and real-time coding interviews.</li>
+      <li>Evaluate candidates
+      with live coding sessions, track their problem-solving approach.</li>
+      <li>Communicate instantly without external tools.</li>
+      <li>And get instant help while coding together in a
+      shared environment.</li>
+
+    </ul>
+  </div>
+</div>
+    </section>
+
+
+    {/* page quote */}
+    <section className="quote">
+      <h1>Join Code Sphere and build together.</h1>
+    </section>
+
+    {/* footer */}
+    <footer className="footer">
+      <p>© 2026 Code Sphere</p>
+
+      <div>
+        <a href="/about">About</a>
+        <a href="/docs">Docs</a>
+        <a href="/contact">Contact</a>
+      </div>
+    </footer>
+
+
     </>
   );
 };
 
-export default welcomePage;
+export default LandingPage;
